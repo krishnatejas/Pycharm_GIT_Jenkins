6da1e462201959410cb7dc53699d348e6a8fec0b
krishnatejas <krishnatejas05@yahoo.com>
2023-03-02 18:58:42 +0530

    New function and testing

--- Hello World.py
+++ Hello World.py
@@ -2,7 +2,10 @@ def add_num(a, b):
     print("function started")
     return a + b
 
+def sub_(a,b):
+    return a - b
 
 if __name__ == '__main__':
     print("adding 2 number: ", add_num(5, 5))
     print("say hello")
+    print("Sub 2 number: ", sub_(5, 1))
