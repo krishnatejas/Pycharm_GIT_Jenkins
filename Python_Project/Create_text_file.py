4b07c4f3fd4c8d81136c61966f27bc7dece4c0dd
krishnatejas <krishnatejas05@yahoo.com>
2023-03-03 14:26:45 +0530

    2nd commit

--- Python_Project/Create_text_file.py
+++ Python_Project/Create_text_file.py
@@ -1,8 +1,9 @@
 def file_creation():
-    with open(r"D:\temp\file.txt",'w') as f:
+    with open(r"D:\temp\file.txt", 'w') as f:
         f.write("Welcome Note")
         print("file created")
 
 
 if __name__ == '__main__':
     file_creation()
+    print("file creation done")
